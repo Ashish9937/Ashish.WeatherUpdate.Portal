0910c494a1f999a47a7840bae6d63c60573e0efa
Ashish9937 <69027728+Ashish9937@users.noreply.github.com>
2021-07-04 18:30:37 +0530

    done changes in contactPHP file

--- contactPHP.php
+++ contactPHP.php
@@ -1,17 +1,14 @@
 <?php
-$name=$_POST['name'];
-$email=$_POST['email'];
-$message=$_POST['message'];
+//get data from form  
 
-$email_from='ashishagrahari98@gmail.com';
-$email_subject="New Submission";
-$email_body="User Name: $name.\n".
-            "User Email: $email.\n".      
-              "User Message: $message.\n";
-
-$to="ashishagrahari98@gmail.com";              
-$headers="From: $email_from\r\n";
-$headers .="Reply-To: $email \r\n";
-mail($to,$email_subject,$email_body,$headers);
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "ashishagrahari98@gmail.com";
+$subject = "Mail From Weather-website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+mail($to,$subject,$txt,$headers);
 header("Location:contact.html");
 ?>
